0d99d6eb649998eb567807a7321182900f87cfac
806gw <shjea4364@naver.com>
2025-05-06 21:33:41 +0900

    feat: postWithToken for signIn and signUp API requests

---
+++ src/api/auth.ts
@@ -0,0 +1,20 @@
+import type { signInType, signUpType } from "../types";
+import { CommonAPI } from "./common";
+
+export const postWithToken = async (
+    accessToken: string | null,
+    url: string,
+    data: signInType | signUpType
+  ): Promise<any> => {
+    try {
+      const response = await CommonAPI.post(url, data, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("POST Error: ", error);
+      throw error;
+    }
+};
---
+++ src/types/auth.types.ts
@@ -0,0 +1,11 @@
+export type signInType = {
+	login: 'string'
+	password: 'string'
+}
+
+export type signUpType = {
+	email: 'string'
+	login: 'string'
+	password: 'string'
+	bio: 'string'
+}
